2b7b89d9403bc11d68d006d7b91e0d8ea9e978de
two-thirty-seven <83845920+two-thirty-seven@users.noreply.github.com>
2024-01-12 17:28:01 +1300

    Adds login bits

--- Features/Login/LoginEndpoints.cs
+++ Features/Login/LoginEndpoints.cs
@@ -6,9 +6,24 @@ public static class LoginEndpoints
 {
     public static WebApplication MapLoginEndpoints(this WebApplication app)
     {
-        app.MapPost("/login/check", ([FromForm]string Email) => new RazorComponentResult<CustomerOptions>(new { Email = Email }));
+        app.MapGet("/login/check", (string Email) => {
+            if (string.IsNullOrEmpty(Email)){
+                return Results.Content("");
+            }            
+            if (Email == "test.user@gmail.com")
+            {
+                string[] customers = { "Customer01", "Customer02", "Customer03", "Customer04" };
+                return new RazorComponentResult<CustomerSelect>(new { Customers = customers });
+            }
+            else
+            {
+                return Results.Content("Email not recognised");
+            }            
+        });
 
-        app.MapPost("/login/customer", ([FromForm]string Email) => new RazorComponentResult<SubmitButton>());
+        app.MapGet("/login/customer", (string Customer) => new RazorComponentResult<SubmitButton>(new { Customer }));
+
+        app.MapGet("/login/go", (HttpContext context) => Results.Content("Welcome"));
 
         return app;
     }
