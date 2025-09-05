e32615cdf2ad000cf4fd1e136b1aacc77aa310d1
paul.taylor <paul.taylor@orchardrock.co.uk>
2025-09-05 20:17:21 +0100

    Removed DaisyUI themes to give full control back to Tailwind. Refined Tailwind themes in global.css to organise colours into hue saturation and light as separate variables.

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,78 +15,72 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        //Brand Colours
         'primarycol': 'var(--primarycol)',
         'secondarycol': 'var(--secondarycol)',
-        'background': 'var(--background)',
-        'foreground': 'var(--foreground)',
-        'muted': 'var(--muted)',
-        'muted-foreground': 'var(--muted-foreground)',
-        'card': 'var(--card)',
-        'border': 'var(--border)',
+        'accentcol': 'var(--accentcol)',
+        'neutralcol': 'var(--neutralcol)',
+        'primarycol-foreground': 'var(--primarycol-foreground)',
+        'secondarycol-foreground': 'var(--secondarycol-foreground)',
+        'accentcol-foreground': 'var(--accentcol-foreground)',
+        'neutralcol-foreground': 'var(--neutralcol-foreground)',
+        //Background Colors
+        'background': 'var(--base)',
         'base': 'var(--base)',
-        'base-50': 'var(--base-50)',
         'base-100': 'var(--base-100)',
-        'base-150': 'var(--base-150)',
         'base-200': 'var(--base-200)',
-        'base-250': 'var(--base-250)',
         'base-300': 'var(--base-300)',
-        'base-350': 'var(--base-350)',
         'base-400': 'var(--base-400)',
-        'base-450': 'var(--base-450)',
         'base-500': 'var(--base-500)',
-        'base-550': 'var(--base-550)',
-        'base-600': 'var(--base-600)',
-        'base-650': 'var(--base-650)',
-        'base-700': 'var(--base-700)',
-        'base-750': 'var(--base-750)',
-        'base-800': 'var(--base-800)',
-        'base-850': 'var(--base-850)',
-        'base-900': 'var(--base-900)',
-        'base-950': 'var(--base-950)',
-
+        'card': 'var(--card)',
+        //Text Colors
+        'foreground': 'var(--foreground)',
+        'foreground-muted': 'var(--muted-foreground)',
+        'base-foreground': 'var(--base-foreground)',
+        'base-100-foreground': 'var(--base-100-foreground)',
+        'base-200-foreground': 'var(--base-200-foreground)',
+        'base-300-foreground': 'var(--base-300-foreground)',
+        'base-400-foreground': 'var(--base-400-foreground)',
+        'base-500-foreground': 'var(--base-500-foreground)',
+        'card-foreground': 'var(--card-foreground)',
+        //Borders
+        'border': 'var(--border)',
+        'border-muted': 'var(--border-muted)',
+        'inputborder': 'var(--inputborder)',
+        'inputborderonfocus': 'var(--inputborderonfocus)',
+        //Status Colors
+        'danger': 'var(--danger)',
+        'warning': 'var(--warning)',
+        'success': 'var(--success)',
+        'info': 'var(--info)',
+        //prevent use of some standard Tailwind and DaisyUI colors
+        'muted': 'var(--danger)',
+        'primary': 'var(--danger)', //make red,
+        'secondary': 'var(--danger)',
+        'accent': 'var(--danger)',
+        'neutral': 'var(--danger)',
       }
     },
   },
   safelist: [
+    // Group: Collapse
     'collapse',
     'collapse-arrow',
     'collapse-title',
     'collapse-content',
+    // Group: Accordion
     'accordion',
     'accordion-item',
     'accordion-title',
     'accordion-content',
+    // Group: DaisyUI Prefix
     { pattern: /^d-/ }, // Safelist all DaisyUI classes with the 'd-' prefix 
-    // add any DaisyUI component classes you need
   ],
   plugins: [
     require('daisyui'),
   ],
   daisyui: {
-    themes: [
-      {
-        "light": {
-          "primary": "hsl(330 100% 60%)",          // Your pink primary color
-          "secondary": "hsl(200 100% 70%)",        // Your blue secondary color
-          "accent": "#37cdbe",
-          "neutral": "hsl(0 0% 30%)",
-          "info": "#3abff8",
-          "success": "#36d399",
-          "warning": "#fbbd23",
-          "error": "#f87272",
-        },
-        "dark": {
-          "primary": "#ff5df1",          // Your pink primary color
-          "secondary": "#46b5ff",        // Your blue secondary color
-          "accent": "#37cdbe",
-          "neutral": "hsl(0 0% 70%)",
-          "info": "#3abff8",
-          "success": "#36d399",
-          "warning": "#fbbd23",
-          "error": "#f87272",
-        },
-      },
-      "cupcake"], // Add themes you want
+    themes: [], // Add themes you want
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
